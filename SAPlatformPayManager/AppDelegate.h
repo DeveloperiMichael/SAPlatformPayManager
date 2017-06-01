83ce0f70920564dbf4110a9a8a3b15466ebb7d4d
张炯 <michael.zhang@wwwarehouse.com>
2017-06-01 10:47:55 +0800

    Initial Commit

---
+++ SAPlatformPayManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SAPlatformPayManager
+//
+//  Created by 张炯 on 17/6/1.
+//  Copyright © 2017年 张炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SAPlatformPayManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SAPlatformPayManager
+//
+//  Created by 张炯 on 17/6/1.
+//  Copyright © 2017年 张炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
